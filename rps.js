b7692cd24c8476c69ada30723efa14968a15f82e
justinnn <orochitachi13@gmail.com>
2022-01-09 20:53:24 +1000

    added list and player win with rock

--- rps.js
+++ rps.js
@@ -1,5 +1,8 @@
 let hands = ["Rock", "Paper", "Scissors"];
 let computerHand;
+const list = document.createElement('ul');
+const ul = document.getElementById('results');
+const br = document.createElement("br");
 
 
     
@@ -13,24 +16,48 @@ document.getElementById('paper').addEventListener("click", function() {
 
 document.getElementById('scissors').addEventListener("click", function() {
     game("Scissors");
+    
 });
+
+function playerdraw() {
+    list.textContent +="Draw!";
+    
+    ul.appendChild(list);//add item to the list
+    ul.appendChild(br);
+};
+
+function playerwin() {
+    list.textContent +="Player won!";
+    
+    ul.appendChild(list);//add item to the list
+    ul.appendChild(br);
+};
+
+function playerlose() {
+    list.textContent +="Player lost!";
+    
+    ul.appendChild(list);//add item to the list
+    ul.appendChild(br);
+};
 
 function computerPlay() { // To generate Computer's Hand for the round
     computerHand = hands[Math.floor(Math.random() * 3)];
-}
+};
 
 function game(playerHand) { //
         computerPlay(computerHand);
         if (computerHand === playerHand) {
-            alert("Both players chose " +playerHand+ "!");
-            alert("It's a draw!");
+            alert("Both players chose " +playerHand+ "! \nIt's a draw!");
+            playerdraw();
         } else if (
-                (playerHand === "Rock" && computerHand === "{Scissors}" ) || 
+                (playerHand === "Rock" && computerHand === "Scissors" ) || 
                 (playerHand === "Scissors" && computerHand === "Paper" ) || 
                 (playerHand === "Paper" && computerHand === "Rock" )) 
                 {
             alert("Player chose " +playerHand+ " while computer chose " +computerHand+ "! \nYou win! " +playerHand +" beats " +computerHand +"!");
+            playerwin();
         } else {
             alert("Player chose " +playerHand+ " while computer chose " +computerHand+ "! \nYou lose! " +computerHand +" beats " +playerHand +"!");
+            playerlose();
         }
-}
+};
